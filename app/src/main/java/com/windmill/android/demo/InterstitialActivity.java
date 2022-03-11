a93cfdbce3274ac32400bf3bc72f2e8d0a04a27c
lance <liuyuabnn@qq.com>
2022-03-11 19:20:47 +0800

    change

--- app/src/main/java/com/windmill/android/demo/InterstitialActivity.java
+++ app/src/main/java/com/windmill/android/demo/InterstitialActivity.java
@@ -150,7 +150,7 @@ public class InterstitialActivity extends Activity implements WMInterstitialAdLi
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Log.d("lance", "------onItemSelected------" + position);
         selectedId = position;
-        if (selectedId < 4) {//SigMob、Unity、Vungle不支持半屏
+        if (selectedId < 3) {//SigMob、Unity、Vungle不支持半屏
             halfScreen.setVisibility(View.GONE);
         } else {
             halfScreen.setVisibility(View.VISIBLE);
