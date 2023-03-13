c410be746bf58e8605c57aa500dc3a2ab7cab010
xiaoxiao <xiao2.xiao@happyelements.com>
2023-03-13 10:13:16 +0800

    delete 多余draw代码位数据 & 新增copy复制按钮

--- app/src/main/java/com/windmill/android/demo/log/ExpandAdapter.java
+++ app/src/main/java/com/windmill/android/demo/log/ExpandAdapter.java
@@ -1,11 +1,16 @@
 package com.windmill.android.demo.log;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,10 +27,12 @@ public class ExpandAdapter extends BaseAdapter {
 
     private List<CallBackItem> mData;
     private Context mContext;
+    ClipboardManager manager;
 
     public ExpandAdapter(Context context, List<CallBackItem> data) {
         this.mContext = context;
         this.mData = data;
+        manager = (ClipboardManager) context.getSystemService(CLIPBOARD_SERVICE);
     }
 
     @Override
@@ -65,14 +72,24 @@ public class ExpandAdapter extends BaseAdapter {
         if (TextUtils.isEmpty(callItem.getChild_text())) {
             viewHolder.expandImage.setVisibility(View.GONE);
             viewHolder.infoText.setVisibility(View.GONE);
+            viewHolder.btnCopy.setVisibility(View.GONE);
         } else {
             viewHolder.infoText.setText(callItem.getChild_text());
             viewHolder.expandImage.setVisibility(View.VISIBLE);
             if (callItem.is_expand()) {
                 viewHolder.infoText.setVisibility(View.VISIBLE);
+                viewHolder.btnCopy.setVisibility(View.VISIBLE);
+                viewHolder.btnCopy.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String content = viewHolder.infoText.getText().toString();
+                        manager.setPrimaryClip(ClipData.newPlainText("", content));
+                    }
+                });
                 viewHolder.expandImage.setImageDrawable(ContextCompat.getDrawable(mContext, R.mipmap.image_arrow_down));
             } else {
                 viewHolder.infoText.setVisibility(View.GONE);
+                viewHolder.btnCopy.setVisibility(View.GONE);
                 viewHolder.expandImage.setImageDrawable(ContextCompat.getDrawable(mContext, R.mipmap.image_arrow_right));
             }
         }
@@ -85,11 +102,13 @@ public class ExpandAdapter extends BaseAdapter {
         TextView callBackText;
         ImageView expandImage;
         TextView infoText;
+        Button btnCopy;
 
         public MyViewHolder(View convertView) {
             callBackText = (TextView) convertView.findViewById(R.id.tv_log);
             expandImage = (ImageView) convertView.findViewById(R.id.iv_log);
             infoText = (TextView) convertView.findViewById(R.id.child_info);
+            btnCopy = (Button) convertView.findViewById(R.id.btn_copy);
         }
     }
 
